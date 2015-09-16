176140b753fc5031410d66e9488cfa8cf02b174d
unknown <rasira867@gmail.com>
2015-09-16 18:17:16 +0900

    three

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root  'users#index'
-  resources :users
+  root :to => 'users#index'
+ 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

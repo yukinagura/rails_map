0b33d165349ac2c4aa7805953bd31aa96c09c82a
unknown <rasira867@gmail.com>
2015-09-17 15:55:08 +0900

    go

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    @lone = User.find(2)
-  @ash = Gmaps4rails.build_markers(@lone) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
-      marker.picture({
-           "url" => "/assets/doubutu1.png",
-           "width" =>  80,
-           "height" => 80})
-      marker.infowindow user.description
-    end
+
 
   @alone = User.find(1)
   @hash = Gmaps4rails.build_markers(@alone) do |user, marker|
